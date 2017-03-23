743b169e496cf342b83ee223af7882c4ff834a73
Justin Zhou <justinzhou93@gmail.com>
2017-03-23 15:17:11 -0400

    feat(login/out): can login and logout now, from both front and back ends. also resets session

--- server/api/users.js
+++ server/api/users.js
@@ -63,7 +63,7 @@ router.get('/profile', (req, res, next) => {
       });
       Promise.all([topArtists, topSongs, userPlaylists, userProfile])
       .spread((foundTopArtists, foundTopSongs, foundUserPlaylists, foundUserProfile) => {
-        console.log('myplaylists: ', foundUserPlaylists);
+        console.log('myplaylists: ', foundTopSongs);
         var artistlist = foundTopArtists.items.map(artistObj => {
           return Artist.findOrCreate({
             where: {
@@ -92,20 +92,21 @@ router.get('/profile', (req, res, next) => {
         const p1 = Promise.all(artistlist);
         const p2 = Promise.all(songlist);
         const p3 = Promise.all(playlistlist);
-
+        console.log('p1: ', p1);
         Promise.all([p1, p2, p3])
         .then(arrayofitems => {
-          console.log(arrayofitems);
+          console.log('arrayofitems:', arrayofitems[0]);
           const updatedArrOfSongs = arrayofitems[1].map(item => {
-            console.log('item inside of updated songs: ', item);
+            console.log('item inside of updated songs: ', item[0]);
             item[0].setUsers(foundUser);
           });
           const updatedArrofPlaylists = arrayofitems[2].map(item => {
             item[0].setUser(foundUser);
           });
-          return Promise.all([updatedArrOfSongs, updatedArrofPlaylists]);
+          return Promise.all([updatedArrOfSongs, updatedArrofPlaylists])
+          // NOTE array of items = [artists, songs, playlists]
+          .then(() => res.status(201).send([arrayofitems[0], arrayofitems[1], arrayofitems[2], foundUserProfile]));
         })
-        .then(() => res.status(201).send([artistlist, songlist, playlistlist, foundUserProfile]))
         .catch(next);
       })
       .catch(next);
