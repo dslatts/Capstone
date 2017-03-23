7ff644b16d0f0e9cc1bea95e806bc67ed03e75c9
Joseph Angelino <angelinojoe@gmail.com>
2017-03-23 14:18:44 -0400

    feat(profile page): initial render

--- browser/components/Profile.js
+++ browser/components/Profile.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import HeaderContainer from '../containers/HeaderContainer';
 export default class Profile extends Component {
 constructor(props){
   super(props);
@@ -7,6 +7,7 @@ constructor(props){
   render () {
     return (
     <div>
+      <HeaderContainer />
       <img src={this.props.currentUser[3].images[0].url} />
       <p>{this.props.currentUser[3].display_name}</p>
     </div>);
