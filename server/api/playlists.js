aedf99a09cb7ec9fbd11ff2ea6cf38f0cdad2a0f
Justin Zhou <justinzhou93@gmail.com>
2017-03-23 12:57:58 -0400

    feat(routes):should be able to post songs to any playlist now. will just append. can't test for now, because don't know how to send post request without having auth attached, but logically speaking, should work

--- server/api/playlists.js
+++ server/api/playlists.js
@@ -61,8 +61,39 @@ router.get('/:playlistName', (req, res, next) => {
   }
 });
 
-// router.post('/:playlistName/addSongs', (req, res, next) => {
-//
-// });
+router.post('/:playlistName/addSongs', (req, res, next) => {
+  if (!req.session.passport){
+    const err = new Error('need to be logged in to use this function');
+    err.status = 403;
+    next(err);
+  } else {
+    // returns the result at the end of this promise chain. should be the created playlist object
+    // with the name, spotify ID and the owner of this playlist
+    return User.findOne({
+      where: {
+        name: req.session.passport.user
+      }
+    })
+    .then(foundUser => {
+      // api.setAccessToken('fill in token');
+      // api.createPlaylist('username', {name: `${req.params.playlistName}`, public: false});
+      return pRequest({
+        url: `https://api.spotify.com/v1/users/${req.session.passport.user}/playlists/${req.body.playlistId}/tracks`,
+        method: 'POST',
+        json: true,
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${foundUser.authToken}`
+        },
+        body: {
+          tracks: req.body.tracks.map(trackId => 'spotify:track:' + trackId).join(','),
+          public: false
+        }
+      })
+      .then(snapshot => {console.log('this returns an id of the snapshot of the playlist. not sure what to do with id...');});
+    })
+    .catch(next);
+  }
+});
 
 module.exports = router;
