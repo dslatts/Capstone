24ba61e120ace14539bb244a27df1a94a4e0eda5
Joseph Angelino <angelinojoe@gmail.com>
2017-03-25 18:26:19 -0400

    feat(profile): renders song history

--- browser/components/History.js
+++ browser/components/History.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
+//used in history, if multiple artists on a song, creates a string 'artist1 feat. artist2, artist3, etc'
+  var createArtistString = function(artistArray){
+    var featured = [];
+    if (artistArray.length === 1){
+      return artistArray[0].name;
+    }
+    else {
+      featured = artistArray.slice(1).map((artist) => artist.name);
+      return `${artistArray[0].name} feat. ${featured.join(', ')}`;
+    }
+  };
 export default function History (props) {
   return (
     <div>
-      <p>Placeholder for History</p>
+      <p>Recent History</p>
+      <table>
+          <thead>
+            <tr>
+                <th>Title</th>
+                <th>Artist</th>
+            </tr>
+          </thead>
+          <tbody>
+              {props.userHistory && props.userHistory.map((item) => {
+              return (
+                <tr key={item.track.id}>
+                  <td>{item.track.name}</td>
+                  <td>{createArtistString(item.track.artists)}</td>
+                </tr>);
+              })}
+          </tbody>
+      </table>
+
+
     </div>);
 
 }
