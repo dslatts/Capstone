e5f2c7cc94a5a1e2932c037a2c697e0ebbf6f346
Joseph Angelino <angelinojoe@gmail.com>
2017-03-26 16:43:42 -0400

    feat(playlist): links to spotify in new tab

--- browser/components/Playlist.js
+++ browser/components/Playlist.js
@@ -7,7 +7,7 @@ export default function Playlist (props) {
     <div>
       <HeaderContainer />
       <h1>Playlist Successfully Posted!</h1>
-      <p>Check it out <a href={`https://play.spotify.com/user/${props.currentUser[3].id}/playlist/${props.currentUser[2][0][0].spotifyId}`}>here</a></p>
+      <p>Check it out <a target="_blank" rel="noopener noreferrer" href={`https://play.spotify.com/user/${props.currentUser[3].id}/playlist/${props.currentUser[2][0][0].spotifyId}`}>here</a></p>
       <Link to={'/'}><button>Start Over!</button></Link>
     </div>);
 }
