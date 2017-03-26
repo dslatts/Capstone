dca828ffc0708b382cffc0ea1effbf1959761687
Joseph Angelino <angelinojoe@gmail.com>
2017-03-26 16:32:29 -0400

    feat(Playlist): posting playist redirects to confirmation page

--- browser/index.js
+++ browser/index.js
@@ -4,23 +4,25 @@ import React from 'react';
 import {Router, Route, browserHistory} from 'react-router';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import Sunburst from './datavis/Sunburst.js'
+import Sunburst from './datavis/Sunburst.js';
 
 import store from './store';
 import {fetchUser} from './actions/user';
 import HomeContainer from './containers/HomeContainer';
 import ProfileContainer from './containers/ProfileContainer';
+import PlaylistContainer from './containers/PlaylistContainer';
 
-const onHomeEnter = function(){
+const refreshUser = function(){
   fetchUser()(store.dispatch);
 };
 
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" onEnter={onHomeEnter} component={HomeContainer} />
+      <Route path="/" onEnter={refreshUser} component={HomeContainer} />
        <Route path="/tests" component={Sunburst} />
       <Route path="/:username/profile" component={ProfileContainer} />
+      <Route path="/:playlist/confirm" onEnter={refreshUser} component={PlaylistContainer} />
     </Router>
   </Provider>,
   document.getElementById('app')
