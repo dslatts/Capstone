7352fcb1e5cfaffe564dd5e07522cb680b6e9492
Joe Angelino <angelinojoe@gmail.com>
2017-03-27 21:53:32 -0400

    Playlist update (#72)
    * feat(playlist): updates the store with all playlists on profile enter
    
    * bug fix: form inside form warning
    
    * feat(playlists): renders list of users playlist on profile AND NOW INCORPORATES BULMA

--- browser/components/Playlist.js
+++ browser/components/Playlist.js
@@ -3,11 +3,18 @@ import HeaderContainer from '../containers/HeaderContainer';
 import {Link} from 'react-router';
 
 export default function Playlist (props) {
+  var playlistURL = '';
+  if (props.currentUser.playlists){
+    playlistURL = `https://play.spotify.com/user/${props.currentUser.spotifyProfile.id}/playlist/${props.currentUser.playlists[0][0].spotifyId}`;
+  }
+  else {
+    playlistURL = `https://play.spotify.com/user/${props.currentUser.spotifyProfile.id}`;
+  }
   return (
     <div>
       <HeaderContainer />
       <h1>Playlist Successfully Posted!</h1>
-      <p>Check it out <a target="_blank" rel="noopener noreferrer" href={`https://play.spotify.com/user/${props.currentUser.spotifyProfile.id}/playlist/${props.currentUser.spotifyProfile.id}`}>here</a></p>
+      <p>Check it out <a target="_blank" rel="noopener noreferrer" href={playlistURL}>here</a></p>*/}
       <Link to={'/'}><button>Start Over!</button></Link>
     </div>);
 }
--- browser/index.js
+++ browser/index.js
@@ -7,7 +7,7 @@ import {Provider} from 'react-redux';
 import Sunburst from './datavis/Sunburst.js';
 
 import store from './store';
-import {fetchUser} from './actions/user';
+import {fetchUser, fetchPlaylist} from './actions/user';
 import HomeContainer from './containers/HomeContainer';
 import ProfileContainer from './containers/ProfileContainer';
 import PlaylistContainer from './containers/PlaylistContainer';
@@ -15,14 +15,17 @@ import PlaylistContainer from './containers/PlaylistContainer';
 const refreshUser = function(){
   fetchUser()(store.dispatch);
 };
+const loadPlaylists = function(){
+  fetchPlaylist()(store.dispatch);
+};
 
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" onEnter={refreshUser} component={HomeContainer} />
-       <Route path="/tests" component={Sunburst} />
-      <Route path="/:username/profile" component={ProfileContainer} />
-      <Route path="/:playlist/confirm" onEnter={refreshUser} component={PlaylistContainer} />
+      <Route path="/tests" component={Sunburst} />
+      <Route path="/:username/profile" onEnter={loadPlaylists} component={ProfileContainer} />
+      <Route path="/:playlist/confirm" onEnter={loadPlaylists} component={PlaylistContainer} />
     </Router>
   </Provider>,
   document.getElementById('app')
