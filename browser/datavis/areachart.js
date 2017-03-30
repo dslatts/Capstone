6ed802eee2fa7a07349b80990e02972d11b6bfb8
Justin Zhou <justinzhou93@gmail.com>
2017-03-29 22:25:23 -0400

    fix(minor): minor changes

--- browser/datavis/areachart.js
+++ browser/datavis/areachart.js
@@ -30,6 +30,7 @@ export default class TimelineChart extends Component {
     let myArr = [];
     let axisTicker = 5;
     let axisLength = Math.round(this.props.currentUser.localProfile.histories.length/5)
+    const sortedHistories = this.props.currentUser.localProfile.histories.slice().sort((a,b) => a.id - b.id);
     const makeAxisValues = function(){
       for (var i = 0; i < axisLength; i++) {
         myArr.push(axisTicker)
@@ -40,7 +41,7 @@ export default class TimelineChart extends Component {
     const chartStyle = { parent: {minWidth: "100%", marginLeft: "10%"}};
     return (
       <div>
-          <VictoryChart width={1500} height={500} style={chartStyle} scale={{x: "time"}} domain={{ y: [0, 1]}}
+          <VictoryChart width={1200} height={400} style={chartStyle} scale={{x: "time"}} domain={{ y: [0, 1]}}
             containerComponent={
               <VictoryZoomContainer responsive={false}
                 dimension="x"
@@ -54,7 +55,7 @@ export default class TimelineChart extends Component {
                 data: {stroke: "tomato"}
               }}
               data={
-                this.props.currentUser.localProfile.histories.map(function(val, index){
+                sortedHistories.map(function(val, index){
                   return {x: new Date(val.date.slice(0, 4), val.date.slice(5, 7) - 1, val.date.slice(8, 10), val.date.slice(11, 13), val.date.slice(14, 16)),  y: val.valence}
                 })
               }
@@ -64,7 +65,7 @@ export default class TimelineChart extends Component {
 
           <VictoryChart
             padding={{top: 0, left: 50, right: 50, bottom: 30}}
-            width={1500} height={200} style={chartStyle} domain={{ y: [0, 1]}} scale={{x: "time"}}
+            width={1200} height={200} style={chartStyle} domain={{ y: [0, 1]}} scale={{x: "time"}}
             containerComponent={
               <VictoryBrushContainer responsive={false}
                 dimension="x"
@@ -78,7 +79,7 @@ export default class TimelineChart extends Component {
                 data: {stroke: "tomato"}
               }}
               data={
-                this.props.currentUser.localProfile.histories.map(function(val, index){
+                sortedHistories.map(function(val, index){
                   return {x: new Date(val.date.slice(0, 4), val.date.slice(5, 7) - 1, val.date.slice(8, 10), val.date.slice(11, 13), val.date.slice(14, 16)),  y: val.valence}
                 })
               }
